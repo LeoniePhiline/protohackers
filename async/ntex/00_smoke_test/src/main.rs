608525086acae2a094450924cf48aaa50a7f7c5c
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2025-05-29 00:43:07 +0200

    chore: draft ntex-neon solution, but it seems weirdly architected 😅

--- async/ntex/00_smoke_test/src/main.rs
+++ async/ntex/00_smoke_test/src/main.rs
@@ -1,7 +1,15 @@
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{self, Result};
 
 fn main() -> Result<()> {
     util::init(0, "Smoke Test", "async/ntex")?;
+
+    let runtime = ntex_neon::Runtime::new()?;
+
+    runtime.block_on(async {
+        //
+
+        eyre::Ok(())
+    })?;
 
     Ok(())
 }
