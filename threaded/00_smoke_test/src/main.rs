b36e5a575ab526a80c357ce7469b1f793fdf713f
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2025-05-28 20:35:38 +0200

    feat: threaded 00 Smoke Test

--- threaded/00_smoke_test/src/main.rs
+++ threaded/00_smoke_test/src/main.rs
@@ -1,7 +1,62 @@
-use color_eyre::eyre::Result;
+use std::{
+    io::{Read, Write},
+    net::{Ipv4Addr, TcpListener, TcpStream},
+};
+
+use color_eyre::eyre::{self, Result};
+use tracing::{debug, info};
 
 fn main() -> Result<()> {
     util::init(0, "Smoke Test", "threaded")?;
+
+    let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, 8080))?;
+
+    info!(local_addr = ?listener.local_addr()?, "Listening.");
+
+    std::thread::scope(|scope| {
+        for stream in listener.incoming() {
+            let stream = stream?;
+
+            info!(peer_addr = ?stream.peer_addr()?, "Connected.");
+
+            scope.spawn(|| {
+                handle_connection(stream)?;
+
+                debug!("Client thread shutdown.");
+
+                eyre::Ok(())
+            });
+        }
+
+        eyre::Ok(())
+    })?;
+
+    info!("Shutdown.");
+
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let mut buf = [0u8; 8192];
+
+    loop {
+        let num_bytes_read = stream.read(&mut buf)?;
+        if num_bytes_read == 0 {
+            info!("Client disconnected.");
+
+            break;
+        }
+
+        let read = &buf[0..num_bytes_read];
+
+        debug!(
+            num_bytes_read,
+            read = &buf[0..num_bytes_read],
+            as_str = ?String::from_utf8_lossy(read)
+        );
+
+        stream.write_all(read)?;
+    }
 
     Ok(())
 }
