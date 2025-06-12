149df45db5d8b7930b689b74c45a729eae5b38fa
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2025-06-12 22:16:08 +0200

    meetup: implement 00 Smoke Test in std threaded

--- threaded/00_smoke_test/src/main.rs
+++ threaded/00_smoke_test/src/main.rs
@@ -1,7 +1,51 @@
+use std::{
+    io::{Read as Foobar, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
 use color_eyre::eyre::Result;
+use tracing::{error, info, trace};
 
 fn main() -> Result<()> {
     util::init(0, "Smoke Test", "threaded")?;
 
+    // - Accept thread
+    //   - Create a socket
+    // per incoming connection:
+    // - Connection thread
+
+    accept()?;
+
     Ok(())
+}
+
+fn accept() -> Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:10000")?;
+
+    loop {
+        let (stream, address) = listener.accept()?;
+
+        info!(%address, "Connection accepted");
+
+        let _ = thread::spawn(|| {
+            let _ = echo(stream).inspect_err(|err| error!("{err:?}"));
+        });
+    }
+}
+
+fn echo(mut stream: TcpStream) -> Result<()> {
+    let mut buffer = [0u8; 4096];
+
+    loop {
+        let count = stream.read(&mut buffer)?;
+        if count == 0 {
+            return Ok(());
+        }
+
+        let bytes = &buffer[..count];
+        trace!(bytes = %String::from_utf8_lossy(bytes), count, "Bytes read");
+
+        stream.write_all(bytes)?;
+    }
 }
